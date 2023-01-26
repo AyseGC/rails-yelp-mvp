16daca62e17d34b3ef1772ea90c99330d03f8997
Aysegul Cakir <aysegul.cakir@kcl.ac.uk>
2023-01-26 21:29:24 +0000

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :restaurants
+  # get 'restaurants/:restaurant_id/reviews', to: 'reviews#index'
+  # get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  # post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+  # # list
+  # get 'restaurants', to: 'restaurants#index'
+  # # Create
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # # read one
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # # Update
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # # Delete
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
 end
